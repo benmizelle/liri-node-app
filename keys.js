d5512a75ade23f0b9e45c7b2dc0bc542db118290
Ben Mizelle <Ben Mizelle>
2017-03-29 12:02:25 -0400

    getting setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<inhneAAyRDiaYP7busKAkwjxv>',
+  consumer_secret: '<D2n5mBbSutbBSvkXRtGyKfWBklybYht8E5Luj1VrRyQOVJEw8a>',
+  access_token_key: '<847109721366695936-ulYBntxOXS5r9mymuTRJHMePZCh0W8a>',
+  access_token_secret: '<6MMXMWLstovn1ua8xJC3J4E5mAHvpBGecIUDXDNmwgjFR>',
+}
