ae046cffef1283d306eb8790afce5d2ec185ff81
Ben Mizelle <Ben Mizelle>
2017-03-29 23:39:24 -0400

    save before the end

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@
  var getTweets = require("./keys.js");
 // Gets twitter keys from the keys file storing it in a variable.
  var keyList = getTweets.twitterKeys;
-// Requires twitter npm package
+// twitter npm package
  var twitter = require("twitter");
 
  var client = new twitter ({
@@ -23,7 +23,7 @@ console.log("Recent Tweets: " + JSON.stringify(client.get));
 
 // ==================request================= //
 
-// Requires request npm package
+// request npm package
  var request = require("request");
  var movieTitle = process.argv[3]
 if (process.argv[2] === "movie-this") {
@@ -40,7 +40,7 @@ if (process.argv[2] === "movie-this") {
 
 // ====================spotify===================== //
 
-// Requires spotify npm package
+// spotify npm package
 var spotify = require("spotify");
 var songTitle = process.argv[3];
 
@@ -66,13 +66,38 @@ spotify.search({ type: 'track', query: songTitle }, function(err, data) {
 
 	console.log("================================================================================");
 });
+}
 
+// ========================do what it says===================== //
+
+// fs package
+var fs = require("fs");
+
+function readFile() {
+	// reads random.txt file
+fs.readFile("random.txt", "utf8", function(error, data) {
+	var output = data
+if (process.argv[2] === "do-what-it-says") {
+	readFile()
+	spotify.search({ type: 'track', query: output }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // conosle.log() info we need
+    console.log("================================================================================");
+    // log artist name
+    console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+    // log song name
+    console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+    // log preview link of the song
+    console.log("Preview Song: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+    // log album name
+    console.log("Album Name: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+
+	console.log("================================================================================");
+});
 };
 
-
-
-
-
-
-
-
+readFile();
